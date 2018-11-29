ddf592a5d6e36b5753d7f2da12cca7c00adb454a
Caleb Hailey <calebhailey@gmail.com>
2018-11-29 12:15:32 -0800

    updated configs and dockerfiles for GA build

--- scripts/sensu-environment.sh
+++ scripts/sensu-environment.sh
@@ -1,6 +1,6 @@
+export SENSU_BACKEND_URL=http://localhost:8080
 export SENSU_USER=admin
 export SENSU_PASS=P@ssw0rd!
-export SENSU_ORG=default
-export SENSU_ENV=default
-export SENSU_TOKEN=`curl -XGET -u "$SENSU_USER:$SENSU_PASS" -s http://localhost:8080/auth | jq -r ".access_token"`
-sensuctl configure -n --username $SENSU_USER --password $SENSU_PASS --organization $SENSU_ORG --environment $SENSU_ENV
+export SENSU_NAMESPACE=default
+export SENSU_TOKEN=`curl -XGET -u "$SENSU_USER:$SENSU_PASS" -s $SENSU_BACKEND_URL/auth | jq -r ".access_token"`
+sensuctl configure -n --username $SENSU_USER --password $SENSU_PASS --namespace $SENSU_NAMESPACE

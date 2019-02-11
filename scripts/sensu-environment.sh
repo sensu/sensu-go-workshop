a0d5c156930fecb37f90434b8315060d202fd7c4
Caleb Hailey <caleb@sensu.io>
2019-02-11 20:41:29 +0000

    more WIP updates

--- scripts/sensu-environment.sh
+++ scripts/sensu-environment.sh
@@ -3,4 +3,4 @@ export SENSU_USER=admin
 export SENSU_PASS=P@ssw0rd!
 export SENSU_NAMESPACE=default
 export SENSU_TOKEN=`curl -XGET -u "$SENSU_USER:$SENSU_PASS" -s $SENSU_BACKEND_URL/auth | jq -r ".access_token"`
-sensuctl configure -n --username $SENSU_USER --password $SENSU_PASS --namespace $SENSU_NAMESPACE
+sensuctl configure -n --url $SENSU_BACKEND_URL --username $SENSU_USER --password $SENSU_PASS --namespace $SENSU_NAMESPACE
